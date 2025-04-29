d5fc46792be12493eac921a4a1037702e41a9ea7
Hemant Sabale <hemantsabale61@gmail.com>
2025-04-29 08:38:04 +0530

    Add files via upload

---
+++ Classes/class1.py
@@ -0,0 +1,73 @@
+# Comments
+# print("hemant")
+
+"""hemant 
+sabale"""
+
+# Variables in python
+# name = "Hemant"
+# print(name)
+
+# You can not use numbers at variable start
+# You can not use spaces in variables.
+# You should not use special characters in variables.
+
+# Naming Conventions  (You can write variables in python using 3 ways)
+hemantSbale = "fullName" # Camel case 
+HemantSabale = "FullName" # Pascal case
+hemant_sabale = "full_name" # Snake case 
+
+# Data Types in python
+
+#  1 Integer Float Complex - All the numbers excluding decimal places and fraction.
+# a = 12
+# print(type(a))
+
+# 2 Float Float
+# b = 56.8
+# c = 56/2
+# print(type(c))
+
+# 3 Complex
+# d = 34j
+# print(type(d))
+
+# 4 Strings
+string = "hemant 123 abc"
+# print(type(string))
+
+# 5 Boolen
+x = True
+y = False
+# print(type(y))
+
+# Strings and type conversion
+# a = "Hemant Sabale"
+# print(a[0])
+# print(a[-1])
+# print(a[6:13:1])
+
+# Type conversion 
+a = 12
+
+# a = str(a)
+# print(type(a))
+
+b = "12"
+
+# b = int(b)
+# print(type(b))
+
+# c = 0
+
+# print(bool(c))
+
+# d = 12
+
+# print(d/3)
+
+# Input and output 
+name = input("Enter Your Name")
+age = input("Enter Your Age")
+print(f"Welcome {name}! Your Age is {age}")
+
